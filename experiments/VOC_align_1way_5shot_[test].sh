2c290be10877fb2629cef0a5b4b6aac3dc803649
maumruiz <mau.mruiz@hotmail.com>
2021-01-29 16:04:15 -0600

    Experiments gpu

--- experiments/VOC_align_1way_5shot_[test].sh
+++ experiments/VOC_align_1way_5shot_[test].sh
@@ -1,5 +1,5 @@
-python test.py with gpu_id=5 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_0_1way_5shot_[train]/1/snapshots/30000.pth'
-python test.py with gpu_id=5 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_1_1way_5shot_[train]/1/snapshots/30000.pth'
-python test.py with gpu_id=5 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_2_1way_5shot_[train]/1/snapshots/30000.pth'
-python test.py with gpu_id=5 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_3_1way_5shot_[train]/1/snapshots/30000.pth'
-python visualize.py with gpu_id=5 mode='visualize' model.align=True task.n_ways=1 task.n_shots=5
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_0_1way_5shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_1_1way_5shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_2_1way_5shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_3_1way_5shot_[train]/1/snapshots/30000.pth'
+python visualize.py with gpu_id=4 mode='visualize' model.align=True task.n_ways=1 task.n_shots=5
