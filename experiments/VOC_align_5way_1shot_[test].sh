2a16c8489bc9c00aa027769e2b13a16848f65998
maumruiz <mau.mruiz@hotmail.com>
2021-02-04 13:09:47 -0600

    5way1shot test script

---
+++ experiments/VOC_align_5way_1shot_[test].sh
@@ -0,0 +1,5 @@
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_0_5way_1shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_1_5way_1shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_2_5way_1shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=4 mode='test' snapshot='./runs/PANetExt_VOC_align_sets_3_5way_1shot_[train]/1/snapshots/30000.pth'
+python visualize.py with gpu_id=4 mode='visualize' model.align=True task.n_ways=5 task.n_shots=1
