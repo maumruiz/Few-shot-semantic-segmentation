9cb3c9558af17083963e69db14a470924ac3e02d
maumruiz <mau.mruiz@hotmail.com>
2021-02-04 15:20:37 -0600

    Experiment scripts for 5way 5shot

---
+++ experiments/VOC_align_5way_5shot_[train].sh
@@ -0,0 +1,4 @@
+python train.py with gpu_id=4 mode='train' dataset='VOC' label_sets=0 model.align=True task.n_ways=5 task.n_shots=5
+python train.py with gpu_id=4 mode='train' dataset='VOC' label_sets=1 model.align=True task.n_ways=5 task.n_shots=5
+python train.py with gpu_id=4 mode='train' dataset='VOC' label_sets=2 model.align=True task.n_ways=5 task.n_shots=5
+python train.py with gpu_id=4 mode='train' dataset='VOC' label_sets=3 model.align=True task.n_ways=5 task.n_shots=5
